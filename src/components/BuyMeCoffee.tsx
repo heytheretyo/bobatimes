7767abc994a4fd53761971978972b04cf5c71f62
tyo <142723158+heytheretyo@users.noreply.github.com>
2025-03-07 12:39:16 +0300

    feat: update donation link

--- src/components/BuyMeCoffee.tsx
+++ src/components/BuyMeCoffee.tsx
@@ -3,19 +3,30 @@ import { Coffee } from "lucide-react";
 
 export default function BuyMeCoffee() {
   return (
+    // <a
+    //   href="https://www.buymeacoffee.com/arthantyo"
+    //   target="_blank"
+    //   rel="noopener noreferrer"
+    //   className="fixed bottom-4 right-4 z-50"
+    // >
+    //   <Button
+    //     className="rounded-full bg-amber-500 hover:bg-amber-600 text-white shadow-lg"
+    //     size="sm"
+    //   >
+    //     <Coffee className="h-4 w-4" />
+    //     Buy me coffee boba
+    //   </Button>
+    // </a>
     <a
-      href="https://www.buymeacoffee.com/arthantyo"
-      target="_blank"
-      rel="noopener noreferrer"
       className="fixed bottom-4 right-4 z-50"
+      href="https://ko-fi.com/C0C01BLNDI"
+      target="_blank"
     >
-      <Button
-        className="rounded-full bg-amber-500 hover:bg-amber-600 text-white shadow-lg"
-        size="sm"
-      >
-        <Coffee className="h-4 w-4" />
-        Buy me coffee boba
-      </Button>
+      <img
+        className="h-10"
+        src="https://storage.ko-fi.com/cdn/kofi6.png?v=6"
+        alt="Buy Me a Coffee at ko-fi.com"
+      />
     </a>
   );
 }
